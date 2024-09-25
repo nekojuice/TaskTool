be7810401d035f05c854b256929fbe96bf6a1162
Jose Wu <josewu@uictech.com.tw>
2024-09-25 15:26:11 +0800

    build: add primevue, primeflex, sass and test

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import popup from '../views/popup.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
--- src/views/popup.vue
+++ src/views/popup.vue
@@ -1,7 +1,40 @@
-<script setup></script>
+<script setup>
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import DatePicker from 'primevue/datepicker'
+import FloatLabel from 'primevue/floatlabel'
+import { ref } from 'vue'
+
+const data = ref({ time: null })
+
+const openInNewTab = () => {
+  window.open('/index.html', '_blank') // 打開新分頁並加載新路由
+}
+</script>
 <template>
   <div>
-    cathi
+    <Button icon="pi pi-window-maximize" severity="secondary" @click="openInNewTab" />
+
+    <div class="grid">
+      <div class="col mb-3">
+        <FloatLabel>
+          <label for="taskHeader">標題</label>
+          <InputText id="taskHeader" type="text" v-model="value" />
+        </FloatLabel>
+      </div>
+      <div class="col mb-3">
+        <FloatLabel>
+          <label for="taskUrl">連結</label>
+          <InputText id="taskUrl" type="text" v-model="value" />
+        </FloatLabel>
+      </div>
+      <div class="col mb-3">
+        <FloatLabel>
+          <label>時間</label>
+          <DatePicker v-model="data.time" selectionMode="range" :manualInput="false" />
+        </FloatLabel>
+      </div>
+    </div>
   </div>
 </template>
 
