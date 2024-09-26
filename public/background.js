b80f4422565631a214b2f4d6fdcb08ba32b84108
Jose Wu <josewu@uictech.com.tw>
2024-09-26 18:12:47 +0800

    update: update ui

--- public/background.js
+++ public/background.js
@@ -1,3 +1,18 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed')
 })
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'setStorage') {
+    chrome.storage.local.set(message.obj, () => {
+      sendResponse({ status: 'success', message: 'Data saved' })
+    })
+    return true
+  }
+  if (message.action === 'getStorage') {
+    chrome.storage.local.get(message.key, (result) => {
+      sendResponse({ status: 'success', message: result[message.key] })
+    })
+    return true
+  }
+})
--- src/views/popup.vue
+++ src/views/popup.vue
@@ -1,23 +1,219 @@
+<template>
+  <div class="grid h-full w-full">
+    <div class="col-1 p-0 flex justify-content-center flex-wrap">
+      <div class="w-2rem flex justify-content-center align-content-start flex-wrap">
+        <Button
+          class="h-2rem w-2rem flex align-items-center justify-content-center"
+          icon="pi pi-ellipsis-v"
+          severity="secondary"
+          outlined
+          @click="loadTasks"
+        />
+        <Button
+          class="h-2rem w-2rem flex align-items-center justify-content-center"
+          icon="pi pi-save"
+          severity="success"
+          outlined
+          @click="saveTasks"
+        />
+      </div>
+      <div class="w-2rem flex justify-content-center align-content-end flex-wrap">
+        <Button
+          class="h-2rem w-2rem flex align-items-center justify-content-center"
+          icon="pi pi-trash"
+          severity="danger"
+          outlined
+          @click="deleteAllData"
+        />
+        <Button
+          class="h-2rem w-2rem flex align-items-center justify-content-center"
+          icon="pi pi-window-maximize"
+          severity="secondary"
+          outlined
+          @click="openInNewTab"
+        />
+      </div>
+    </div>
+    <div class="col-11">
+      <div class="grid">
+        <div class="col-1">
+          <label for="taskId">編號</label>
+          <InputNumber id="taskId" v-model="id" inputClass="w-3rem" :min="0" size="small" />
+        </div>
+        <div class="col-5">
+          <label for="taskHeader">標題</label>
+          <InputText
+            id="taskHeader"
+            class="w-full"
+            type="text"
+            v-model="data.taskHeader"
+            @change="saveCache()"
+          />
+        </div>
+        <div class="col-6">
+          <label for="taskUrl">連結</label>
+          <InputText
+            id="taskUrl"
+            class="w-full"
+            type="text"
+            v-model="data.taskUrl"
+            @change="saveCache()"
+          />
+        </div>
+        <div class="col-6">
+          <label for="taskBranch">分支</label>
+          <InputText
+            id="taskBranch"
+            class="w-full"
+            type="text"
+            v-model="data.taskBranch"
+            @change="saveCache()"
+          />
+        </div>
+        <div class="col-6">
+          <label class="mr-1">日期</label>
+          <DatePicker
+            class="w-full"
+            v-model="data.date"
+            :manualInput="false"
+            @change="saveCache()"
+          />
+        </div>
+        <div class="col-12">
+          <InputNumber
+            v-model="computedStartTimeHour"
+            inputClass="w-3rem"
+            :min="0"
+            :max="24"
+            size="small"
+          />&nbsp;:&nbsp;
+          <InputNumber
+            v-model="computedStartTimeMinute"
+            inputClass="w-3rem"
+            :min="0"
+            :max="60"
+            size="small"
+          />
+          &nbsp;~&nbsp;
+          <InputNumber
+            v-model="computedEndTimeHour"
+            inputClass="w-3rem"
+            :min="0"
+            :max="24"
+            size="small"
+          />&nbsp;:&nbsp;
+          <InputNumber
+            v-model="computedEndTimeMinute"
+            inputClass="w-3rem"
+            :min="0"
+            :max="60"
+            size="small"
+          />
+          <Slider v-model="data.timeTick" range :step="1" :max="1441" class="w-full" />
+        </div>
+
+        <div class="col-12">
+          <div v-for="task in tasks">
+            <label class="mr-2">#{{ task.id }}</label>
+            <Button class="mr-2 w-5rem">{{ task.data.taskHeader }}</Button>
+            <!-- <label class="mr-2">({{ calEffort(task.data.time) }})</label> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 <script setup>
-import { computed, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import DatePicker from 'primevue/datepicker'
-import Divider from 'primevue/divider'
 import Slider from 'primevue/slider'
 import InputNumber from 'primevue/inputnumber'
 
-let currentTime = new Date()
+onMounted(() => {
+  loadCache()
+  loadTasks()
+})
+
+const tasks = ref([])
+
+const id = ref(0)
 const data = ref({
-  date: currentTime,
+  taskHeader: '',
+  taskUrl: '',
+  taskBranch: '',
+  date: new Date(),
   timeTick: [
-    currentTime.getHours() * 60 + currentTime.getMinutes() - 60,
-    currentTime.getHours() * 60 + currentTime.getMinutes()
+    new Date().getHours() * 60 + new Date().getMinutes() - 60,
+    new Date().getHours() * 60 + new Date().getMinutes()
   ]
 })
 
+const loadCache = () => {
+  chrome.runtime.sendMessage({ action: 'getStorage', key: 'cache' }, (response) => {
+    if (response.message) {
+      data.value = response.message
+    }
+  })
+}
+const saveCache = () => {
+  chrome.runtime.sendMessage({ action: 'setStorage', obj: { cache: data.value } }, (response) => {
+    console.log(response.message)
+  })
+}
+
+const loadTasks = async () => {
+  chrome.runtime.sendMessage({ action: 'getStorage', key: 'tasks' }, (response) => {
+    console.log(response.message)
+    tasks.value = response.message
+  })
+}
+const saveTasks = () => {
+  const index = tasks.value.findIndex((t) => t.id === id.value)
+
+  if (index !== -1) {
+    tasks.value[index].data = data.value
+  } else {
+    const task = {
+      id: id.value,
+      data: data.value
+    }
+    tasks.value.push(task)
+  }
+
+  chrome.runtime.sendMessage({ action: 'setStorage', obj: { tasks: tasks.value } }, (response) => {
+    console.log(response.message)
+  })
+}
+const deleteAllData = () => {
+  // chrome.runtime.sendMessage({ action: 'setStorage', obj: {} }, (response) => {
+  //   console.log(response.message)
+  // })
+}
 const openInNewTab = () => {
-  window.open('/index.html', '_blank') // 打開新分頁並加載新路由
+  window.open('/index.html', '_blank')
+}
+
+// TODO
+const saveTime = () => {}
+
+const calEffort = (timearr) => {
+  let totalMinutes = 0
+
+  timearr.forEach((dayObj) => {
+    Object.values(dayObj).forEach((timeRanges) => {
+      timeRanges.forEach((range) => {
+        const [start, end] = range
+        totalMinutes += end - start
+      })
+    })
+  })
+
+  // 計算人日，1 人日 = 60 分鐘 * 0.125
+  const personDays = (totalMinutes / 60) * 0.125
+
+  return personDays
 }
 
 const computedStartTimeHour = computed({
@@ -79,6 +275,9 @@ const computedEndTimeMinute = computed({
 watch(
   () => data.value.timeTick,
   (timeTick) => {
+    timeTick[0] = timeTick[0] < 0 ? 0 : timeTick[0]
+    timeTick[1] = timeTick[1] > 1440 ? 1440 : timeTick[1]
+
     if (timeTick[0] > 1425) {
       timeTick[0] = 1425
       timeTick[1] = 1440
@@ -89,54 +288,5 @@ watch(
   }
 )
 </script>
-<template>
-  <div class="grid">
-    <div class="col-1">
-      <Button icon="pi pi-window-maximize" severity="secondary" @click="openInNewTab" />
-    </div>
-    <div class="col-1">
-      <Divider layout="vertical" />
-    </div>
-    <div class="col-10">
-      <div class="grid">
-        <div class="col-12 mb-2">
-          <label for="taskHeader">標題</label>
-          <InputText id="taskHeader" class="w-full" type="text" v-model="data.taskHeader" />
-        </div>
-        <div class="col-12 mb-2">
-          <label for="taskUrl">連結</label>
-          <InputText id="taskUrl" class="w-full" type="text" v-model="data.taskUrl" />
-        </div>
-        <div class="col-12">
-          <div>
-            <label class="mr-1">日期</label>
-            <DatePicker class="w-full" v-model="data.date" :manualInput="false" />
-          </div>
-
-          <div>
-            <label class="mr-1">開始</label>
-            <!-- <Button class="h-1rem" label="now" @click="data.time[0] = new Date()" /> -->
-            <InputNumber
-              inputClass="w-5rem"
-              :min="0"
-              :max="24"
-              v-model="computedStartTimeHour"
-            />&nbsp;:&nbsp;
-            <InputNumber inputClass="w-5rem" :min="0" :max="60" v-model="computedStartTimeMinute" />
-            &nbsp;~&nbsp;
-            <InputNumber
-              inputClass="w-5rem"
-              :min="0"
-              :max="24"
-              v-model="computedEndTimeHour"
-            />&nbsp;:&nbsp;
-            <InputNumber inputClass="w-5rem" :min="0" :max="60" v-model="computedEndTimeMinute" />
-          </div>
-          <Slider v-model="data.timeTick" range step="15" max="1440" class="w-full" />
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
 <style scoped></style>
