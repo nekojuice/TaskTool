34cdeaae2e42e186520de427e8590e88ea7d47dd
Jose Wu <josewu@uictech.com.tw>
2024-11-04 18:11:12 +0800

    test: add report template

---
+++ src/components/MonthReport.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+<script setup>
+function getMonthlyWorkPeriods(tasks, year, month) {
+  const monthlyPeriods = [];
+
+  tasks.forEach(task => {
+    task.times.forEach(time => {
+      const [taskYear, taskMonth] = time.date.split('/').map(Number);
+
+      if (taskYear === year && taskMonth === month) {
+        monthlyPeriods.push(...time.periods);
+      }
+    });
+  });
+
+  return processWorkPeriods(monthlyPeriods);
+}
+</script>
+
+<style scoped></style>
